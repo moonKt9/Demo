0dbe8e2d4426e9ec145005d1c27748f26f0fb5bd
Kritika <kritistarsmind@gmail.com>
2022-04-27 15:36:25 +0530

    Added:Baic programs

---
+++ Star1.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+
+public class Star1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int r=5;
+for(int i=1; i<=r; i++)
+{
+	for(int j=1; j<=i; j++)
+	{
+		System.out.println("*");
+	}
+	System.out.println();
+}
+}
+}
